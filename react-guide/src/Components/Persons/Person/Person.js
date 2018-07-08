5630dca20642f0279ae74c8dd6af4db6eb02fd78
Mykolaj <32041427+MykolajKrusser@users.noreply.github.com>
2018-07-08 15:45:58 +0200

    References in 16.3 react version

--- react-guide/src/Components/Persons/Person/Person.js
+++ react-guide/src/Components/Persons/Person/Person.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import classes from './Person.css';
 
 class Person extends Component {
+  constructor(props){
+    super(props);
+    this.inputElement = React.createRef();
+  }
+  componentDidMount(){
+    if(this.props.position === 0){
+      this.inputElement.current.focus();
+    }
+  }
   render() {
     return (
       <div className={classes.Person}>
         <h1 onClick={this.props.click}>Hi I am {this.props.name}</h1>
         <p>and I am {this.props.age} years old</p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+        <input type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+          ref={this.inputElement} //"ref" for focus on first input element
+          />
       </div>
     );    
   }
